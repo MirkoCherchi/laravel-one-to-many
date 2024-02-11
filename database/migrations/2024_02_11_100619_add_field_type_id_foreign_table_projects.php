7f2e03f4be67bab416b12fa4bde0fe93ecb41031
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-02-11 11:56:03 +0100

    feat: random type

--- database/migrations/2024_02_11_100619_add_field_type_id_foreign_table_projects.php
+++ database/migrations/2024_02_11_100619_add_field_type_id_foreign_table_projects.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('projects', function (Blueprint $table) {
             $table->unsignedBigInteger('type_id')->nullable()->after('id');
-            $table->foreign('type_id')->references('id')->on('types');
+            $table->foreign('type_id')->references('id')->on('types')->nullOnDelete();
         });
     }
 
@@ -23,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->dropForeign(['projects_category_id_foreign']);
+            $table->dropForeign(['type_id']);
             $table->dropColumn('type_id');
         });
     }
--- database/seeders/ProjectSeed.php
+++ database/seeders/ProjectSeed.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -15,12 +16,17 @@ class ProjectSeed extends Seeder
      */
     public function run(Faker $faker): void
     {
+        Project::truncate();
+
         for ($i = 0; $i < 20; $i++) {
+
+            $type = Type::inRandomOrder()->first();
             $project = new Project();
 
             $project->title = $faker->sentence(2);
             $project->description = $faker->text();
             $project->slug = Str::of($project->title)->slug('-');
+            $project->type_id = $type->id;
             $project->save();
         }
     }
